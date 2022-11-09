1d346dcb6adbb7151fa4dc1c0473cf29076710a7
lbh <2275949729@qq.com>
2022-11-09 17:31:53 +0800

    merge1

--- src/main/java/com/lbh/git/GitTest.java
+++ src/main/java/com/lbh/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello world!");
         System.out.println("hello world!");
         System.out.println("hello world!");
+        System.out.println("merge1");
     }
 }
