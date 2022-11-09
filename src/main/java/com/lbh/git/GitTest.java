a725fb29c84d568f07c6912a69fa6f1febcb9ad4
lbh <2275949729@qq.com>
2022-11-09 16:10:20 +0800

    first commit

---
+++ src/main/java/com/lbh/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.lbh.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author computer
+ * @Date 2022/11/9 15:55
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
