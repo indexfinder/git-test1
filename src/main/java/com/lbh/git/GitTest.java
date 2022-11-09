aaec912f8bac387477b7f357464ec3392bf02d92
lbh <2275949729@qq.com>
2022-11-09 16:16:47 +0800

    second commit

--- src/main/java/com/lbh/git/GitTest.java
+++ src/main/java/com/lbh/git/GitTest.java
@@ -10,5 +10,6 @@ package com.lbh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
